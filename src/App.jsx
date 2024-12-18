7c04c9ba180faf79618a4fd5d2c0cb7414eef1b3
Abudi <abduselamt47@gmail.com>
2024-12-18 09:31:15 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,33 @@ function App() {
 
   const date = new Date();
   date.setDate(date.getDate() + day);
+  function handleReset() {
+    SetDay(0);
+    setStep(1)
+  }
+
+ 
 
   return (
     <>
       <div className="">
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        {/* <button onClick={() => setStep((c) => c - 1)}>-</button>
         <span>Step:{step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        <button onClick={() => setStep((c) => c + 1)}>+</button> */}
+        <input type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}/>
+          <span>{step}</span>
       </div>
       <div className="">
         <button onClick={() => SetDay((c) => c - step)}>-</button>
-        <span>count:{day}</span>
+        {/* <span>count:{day}</span> */}
+        <input type="text" 
+               value={day}
+               onChange={(e) => SetDay(+e.target.value)}/>
+            
         <button onClick={() => SetDay((c) => c + step)}>+</button>
       </div>
 
@@ -28,6 +44,10 @@ function App() {
           {day} days from today is {date.toDateString()}{" "}
         </h3>
       </div>
+
+     { (day!==0  || step !==1) ? <div>
+        <button type="text" onClick={handleReset}>Reset</button>
+      </div> : null}
     </>
   );
 }
